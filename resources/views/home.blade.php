94eab618766c23b90846065614d5932c8ac1724f
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-07-14 17:41:19 +0200

    nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    </head>
-    <body>
-      {{-- <h1> {{ $title }} </h1> --}}
-
-      
-
-      <a href="/padrino "> padrino</a>
-      <a href="/ciao "> ciao</a>
-
-      <div class="container">
-        <div class="movies">
-          @foreach ($movies as $movie)
-            <div class="card-movie">
-              <p> <strong>Title: </strong> {{ $movie->title }} </p>
-              <p> <strong>Original Title: </strong> {{ $movie->original_title }} </p>
-              <p> <strong>Nationality: </strong> {{ $movie->nationality }} </p>
-              <p> <strong>Date: </strong> {{ $movie->date  }} </p>
-              <p><strong>rating: </strong> {{ $movie->vote  }} </p>
-            </div>
-          @endforeach
-        </div>
-      </div>
 
 
-    </body>
-</html>
+@include('layouts/main')
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a href="#">titles</a>
+        </li>
+        <li>
+            <a href="#">Year</a>
+        </li>
+    </ul>
+</nav>
